2ecddaa7bbaf7a832f0712d42978635716d39a73
Markus Mayer <widemeadows@gmail.com>
2020-04-30 23:13:14 +0200

    Fix missing height map in grid discretization call

--- motion_planning.py
+++ motion_planning.py
@@ -159,7 +159,7 @@ class MotionPlanning(Drone):
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
 
         # Define a grid for a particular altitude and safety margin around obstacles
-        grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
+        grid, heightmap, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
         grid_start = (-north_offset, -east_offset)
